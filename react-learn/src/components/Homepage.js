119040d5b4fe681ad18b223f86f47ab4f7985027
Samerhajj <81771181+Samerhajj@users.noreply.github.com>
2022-09-03 13:35:59 +0300

    test

--- react-learn/src/components/Homepage.js
+++ react-learn/src/components/Homepage.js
@@ -55,7 +55,7 @@ export default function Homepage({name}){
                             <input checked={isChecked} onChange={handleOnChange}
                                 className="form-check-input mt-0" type="checkbox" 
                                 value="" aria-label="Checkbox for following text input"/>
-                            <label >Take Aways</label>
+                            <label >Take Away</label>
                         </div>
                     </div>
 
